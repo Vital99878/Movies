69a858ca02ec17c299cb4ea1bb4b4d886be38db5
Vital <Lihoy84@yandex.ru>
2021-02-04 14:32:26 +0300

    genres styled

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -31,14 +31,17 @@ function Movie({ title, overview, genres, rate, poster_path, release, genres_ids
     const day_number = new Date(date).getDate();
     return `${month} ${day_number}, ${year}`;
   };
+
   const truncate_overview = (text) => `${text.slice(0, 180)} ...`;
 
   return (
     <div className="card">
       <img className="card__poster" src={poster_path} alt="Poster" />
       <div className="card__description">
-        <p className="card__title">{title}</p>
-        <picture className="card__rate">{rate}</picture>
+        <div className="card__title_rate">
+          <p className="card__title">{title}</p>
+          <picture className="card__rate">{rate}</picture>
+        </div>
         <p className="card__release">{release_date(release)}</p>
         <div className="card__genres"> {genres_list}</div>
         <p className="card__overview">{truncate_overview(overview)}</p>
