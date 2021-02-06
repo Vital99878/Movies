fe5a66469d8f679822bc26b953bda3ef04fc2088
Vital <Lihoy84@yandex.ru>
2021-02-06 10:56:09 +0300

    update error warning

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import 'antd/dist/antd.css';
 
 import { Pagination, Spin, Alert } from 'antd';
-import MoviesList from '../MoviesList';
-import Search from '../Search';
-import Tabs from '../Tabs';
-import Movies_Service from '../Api';
+import MoviesList                  from '../MoviesList';
+import Search                      from '../Search';
+import Tabs                        from '../Tabs';
+import Movies_Service              from '../Api';
+
 
 export default class App extends Component {
   movie_service = new Movies_Service();
@@ -22,49 +23,49 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    this.getMovies('return');
+    this.getMovies( 'return' );
     this.getGenres();
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
+  componentDidUpdate( prevProps, prevState, snapshot ) {
     const { search } = this.state;
-    if (search !== prevState.search) {
-      this.getMovies(search);
+    if ( search !== prevState.search ) {
+      this.getMovies( search );
     }
   }
 
-  getMovies(movie) {
+  getMovies( movie ) {
     this.movie_service
-      .get_movies(movie)
-      .then((movies) => {
-        const { movies_pages, quantity_movies } = movies;
-        this.setState({ movies_pages, quantity_movies, loading: false });
-      })
-      .catch(() => this.onError());
+        .get_movies( movie )
+        .then( ( movies ) => {
+          const { movies_pages, quantity_movies } = movies;
+          this.setState( { movies_pages, quantity_movies, loading: false, error: false } );
+        } )
+        .catch( () => this.onError() );
   }
 
   getGenres() {
     this.movie_service
-      .get_genres()
-      .then((genres_ids) => {
-        this.setState({ genres_ids });
-      })
-      .catch(() => this.onError());
+        .get_genres()
+        .then( ( genres_ids ) => {
+          this.setState( { genres_ids } );
+        } )
+        .catch( () => this.onError() );
   }
 
-  change_page_number = (page) => {
-    this.setState({
-      page_number: page - 1,
-    });
+  change_page_number = ( page ) => {
+    this.setState( {
+                     page_number: page - 1,
+                   } );
   };
 
   onError = () => {
-    this.setState({ loading: false, error: true });
+    this.setState( { loading: false, error: true } );
   };
 
-  add_rate = (label) => {
-    if (label.length >= 3) {
-      this.setState(({ todo_list }) => {
+  add_rate = ( label ) => {
+    if ( label.length >= 3 ) {
+      this.setState( ( { todo_list } ) => {
         const new_todo = {
           label,
           created: Date.now(),
@@ -72,31 +73,23 @@ export default class App extends Component {
           status: 'active',
         };
         return {
-          todo_list: [...todo_list, new_todo],
+          todo_list: [ ...todo_list, new_todo ],
         };
-      });
+      } );
     }
   };
 
-  get_search_text = (text) => {
-    this.setState({ search: text, loading: true });
+  get_search_text = ( text ) => {
+    this.setState( { search: text, loading: true } );
   };
 
   render() {
     const { movies_pages, page_number, genres_ids, quantity_movies, loading, error, search } = this.state;
 
-    if (loading) {
+    if ( loading ) {
       return (
         <div className="spinner">
           <Spin size="large" />
-        </div>
-      );
-    }
-
-    if (error) {
-      return (
-        <div>
-          <Alert message="There are no movies with this name" description="Try search another movie" type="warning" />
         </div>
       );
     }
@@ -107,8 +100,19 @@ export default class App extends Component {
           <Tabs />
           <Search search={search} get_search_text={this.get_search_text} />
         </div>
-        <MoviesList movies_pages={movies_pages} page_number={page_number} genres_ids={genres_ids} />
-        <Pagination current={page_number + 1} pageSize={6} onChange={this.change_page_number} total={quantity_movies} />
+        {error ? (
+          <Alert message="There are no movies with this name" description="Try search another movie" type="warning" />
+        ) : (
+          <MoviesList movies_pages={movies_pages} page_number={page_number} genres_ids={genres_ids} />
+        )}
+        { error || (
+          <Pagination
+            current={page_number + 1}
+            pageSize={6}
+            onChange={this.change_page_number}
+            total={quantity_movies}
+          />
+        )}
       </section>
     );
   }
